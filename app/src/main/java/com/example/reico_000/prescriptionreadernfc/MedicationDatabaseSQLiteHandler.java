43e01a7ecd3acc63e5a3c925e994234a1a6a6e77
YosuaSusanto <yosuasusanto@gmail.com>
2015-10-18 02:01:26 +0800

    Add ConsumptionTable to DatabaseHandler

--- app/src/main/java/com/example/reico_000/prescriptionreadernfc/MedicationDatabaseSQLiteHandler.java
+++ app/src/main/java/com/example/reico_000/prescriptionreadernfc/MedicationDatabaseSQLiteHandler.java
@@ -14,7 +14,27 @@ import java.util.List;
  * Created by reico_000 on 14/3/2015.
  */
 public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
-	// All Static variables
+    private static MedicationDatabaseSQLiteHandler sInstance;
+
+    public static synchronized MedicationDatabaseSQLiteHandler getInstance(Context context) {
+        // Use the application context, which will ensure that you
+        // don't accidentally leak an Activity's context.
+        // See this article for more information: http://bit.ly/6LRzfx
+        if (sInstance == null) {
+            sInstance = new MedicationDatabaseSQLiteHandler(context.getApplicationContext());
+        }
+        return sInstance;
+    }
+
+    /**
+     * Constructor should be private to prevent direct instantiation.
+     * Make a call to the static method "getInstance()" instead.
+     */
+    private MedicationDatabaseSQLiteHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // All Static variables
 	// Database Version
 	private static final int DATABASE_VERSION = 1;
 
@@ -26,19 +46,26 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
 
 	// Contacts table name
     public static final String TABLE_MEDICATIONS = "medicationTable";
+    public static final String TABLE_CONSUMPTIONS = "consumptionTable";
 
-	// Contacts Table Columns names
+	// Common columns names
 	public static final String KEY_ID = "_id";
-    public static final String KEY_BRANDNAME = "BrandName";
-    public static final String KEY_GENERICNAME = "GenericName";
-    public static final String KEY_DOSAGEFORM = "DosageForm";
-    public static final String KEY_PERDOSAGE = "PerDosage";
-    public static final String KEY_TOTALDOSAGE = "TotalDosage";
-    public static final String KEY_CONSUMPTIONTIME = "ConsumptionTime";
-    public static final String KEY_PATIENTID = "PatientID";
+
+    // Medications table column names
+    public static final String KEY_BRAND_NAME = "BrandName";
+    public static final String KEY_GENERIC_NAME = "GenericName";
+    public static final String KEY_DOSAGE_FORM = "DosageForm";
+    public static final String KEY_PER_DOSAGE = "PerDosage";
+    public static final String KEY_TOTAL_DOSAGE = "TotalDosage";
+    public static final String KEY_CONSUMPTION_TIME = "ConsumptionTime";
+    public static final String KEY_PATIENT_ID = "PatientID";
     public static final String KEY_ADMINISTRATION = "Administration";
-    public static final String[] ALL_KEYS = new String[] {KEY_ID, KEY_BRANDNAME, KEY_GENERICNAME, KEY_DOSAGEFORM, KEY_PERDOSAGE, KEY_TOTALDOSAGE,
-            KEY_CONSUMPTIONTIME, KEY_PATIENTID, KEY_ADMINISTRATION};
+    public static final String[] ALL_KEYS = new String[] {KEY_ID, KEY_BRAND_NAME, KEY_GENERIC_NAME, KEY_DOSAGE_FORM, KEY_PER_DOSAGE, KEY_TOTAL_DOSAGE,
+            KEY_CONSUMPTION_TIME, KEY_PATIENT_ID, KEY_ADMINISTRATION};
+
+    // Consumptions table column names
+    public static final String KEY_MEDICATION_ID = "MedicationID";
+    public static final String KEY_CONSUMED_AT = "ConsumedAt";
 
     // TODO: Setup your field numbers here (0 = KEY_ROWID, 1=...)
     public static final int COL_BRANDNAME = 1;
@@ -50,34 +77,38 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
     public static final int COL_PATIENTID = 7;
     public static final int COL_ADMINISTRATION = 8;
 
-	public MedicationDatabaseSQLiteHandler(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
-
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		String CREATE_MEDICATION_TABLE = "CREATE TABLE " + TABLE_MEDICATIONS + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_BRANDNAME + " TEXT," + KEY_GENERICNAME + " TEXT," + KEY_DOSAGEFORM + " TEXT," + KEY_PERDOSAGE + " TEXT, " + KEY_TOTALDOSAGE + " TEXT, " + KEY_CONSUMPTIONTIME + " TEXT, " + KEY_PATIENTID + " TEXT, " + KEY_ADMINISTRATION + " TEXT )";
-		db.execSQL(CREATE_MEDICATION_TABLE);
+        String CREATE_MEDICATION_TABLE = "CREATE TABLE " + TABLE_MEDICATIONS + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_BRAND_NAME + " TEXT," + KEY_GENERIC_NAME + " TEXT," + KEY_DOSAGE_FORM + " TEXT," + KEY_PER_DOSAGE + " TEXT, " +
+                KEY_TOTAL_DOSAGE + " TEXT, " + KEY_CONSUMPTION_TIME + " TEXT, " + KEY_PATIENT_ID + " TEXT, " + KEY_ADMINISTRATION + " TEXT )";
+        String CREATE_CONSUMPTION_TABLE = "CREATE TABLE " + TABLE_CONSUMPTIONS + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_MEDICATION_ID + " INTEGER," + KEY_CONSUMED_AT + " TEXT )";
+
+        db.execSQL(CREATE_MEDICATION_TABLE);
+        db.execSQL(CREATE_CONSUMPTION_TABLE);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int i, int i2) {
         // Drop older table if existed
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEDICATIONS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONSUMPTIONS);
         onCreate(db);
     }
-		// Adding new MedicationObject
-		public void addMedication(MedicationObject medicationObject) {
+
+    // Adding new MedicationObject
+    public void addMedication(MedicationObject medicationObject) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(KEY_BRANDNAME, medicationObject.get_brandName()); // Brand Name
-        values.put(KEY_GENERICNAME, medicationObject.get_genericName()); // Generic Name
-        values.put(KEY_DOSAGEFORM, medicationObject.get_dosageForm()); // Dosage Form and Strength
-        values.put(KEY_PERDOSAGE, medicationObject.get_perDosage()); // Pill/Tablet Per Dosage
-        values.put(KEY_TOTALDOSAGE, medicationObject.get_totalDosage()); // Total Dosage
-        values.put(KEY_CONSUMPTIONTIME, medicationObject.get_consumptionTime()); // Time of Consumption
-        values.put(KEY_PATIENTID, medicationObject.get_patientID()); // Patient's ID
+        values.put(KEY_BRAND_NAME, medicationObject.get_brandName()); // Brand Name
+        values.put(KEY_GENERIC_NAME, medicationObject.get_genericName()); // Generic Name
+        values.put(KEY_DOSAGE_FORM, medicationObject.get_dosageForm()); // Dosage Form and Strength
+        values.put(KEY_PER_DOSAGE, medicationObject.get_perDosage()); // Pill/Tablet Per Dosage
+        values.put(KEY_TOTAL_DOSAGE, medicationObject.get_totalDosage()); // Total Dosage
+        values.put(KEY_CONSUMPTION_TIME, medicationObject.get_consumptionTime()); // Time of Consumption
+        values.put(KEY_PATIENT_ID, medicationObject.get_patientID()); // Patient's ID
         values.put(KEY_ADMINISTRATION, medicationObject.get_administration()); // Administration Info
 
         // Inserting Row
@@ -85,32 +116,32 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
         db.close(); // Closing database connection
     }
 
-        // Getting All Medications
-		public List<MedicationObject> getAllMedications() {
-            List<MedicationObject> medicationList = new ArrayList<MedicationObject>();
-            // Select All Query
-            String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS;
+    // Getting All Medications
+    public List<MedicationObject> getAllMedications() {
+        List<MedicationObject> medicationList = new ArrayList<MedicationObject>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS;
 
-            SQLiteDatabase db = this.getWritableDatabase();
-            Cursor cursor = db.rawQuery(selectQuery, null);
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
 
-            // looping through all rows and adding to list
-            if (cursor.moveToFirst()) {
-                do {
-                    MedicationObject medicationObject= new MedicationObject(cursor.getString(1),cursor.getString(2),cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7), cursor.getColumnName(8));
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                MedicationObject medicationObject= new MedicationObject(cursor.getString(1),cursor.getString(2),cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7), cursor.getColumnName(8));
 
-                    //Do I need to put in UID?
-                    medicationObject.set_ID(Integer.parseInt(cursor.getString(0)));
-                    // Adding contact to list
-                    medicationList.add(medicationObject);
-                } while (cursor.moveToNext());
-            }
-
-            cursor.close();
-
-            // return contact list
-            return medicationList;
+                //Do I need to put in UID?
+                medicationObject.set_ID(Integer.parseInt(cursor.getString(0)));
+                // Adding contact to list
+                medicationList.add(medicationObject);
+            } while (cursor.moveToNext());
         }
+
+        cursor.close();
+
+        // return contact list
+        return medicationList;
+    }
 
     public Cursor getAllRows() {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -128,7 +159,7 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
 
     public Cursor getRowsByPatientId(String scan_PatientId) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_PATIENTID + " = '" + scan_PatientId + "'";
+        String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_PATIENT_ID + " = '" + scan_PatientId + "'";
 
         Cursor c = db.rawQuery(selectQuery, null);
         if (c != null) {
@@ -153,8 +184,8 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
 
     public Cursor getByNameAndDosageForm(String scan_BrandName, String scan_DosageForm){
         SQLiteDatabase db = this.getWritableDatabase();
-        String countQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_BRANDNAME + " = '" + scan_BrandName + "' AND " + KEY_DOSAGEFORM + " = '" + scan_DosageForm + "'";
-        Cursor c = db.rawQuery(countQuery, null);
+        String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_BRAND_NAME + " = '" + scan_BrandName + "' AND " + KEY_DOSAGE_FORM + " = '" + scan_DosageForm + "'";
+        Cursor c = db.rawQuery(selectQuery, null);
         if (c != null) {
             c.moveToFirst();
         }
@@ -163,11 +194,11 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
 
     public int getTotalDosage(String scan_BrandName, String scan_DosageForm){
         SQLiteDatabase db = this.getWritableDatabase();
-        String countQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_BRANDNAME + " = '" + scan_BrandName + "' AND " + KEY_DOSAGEFORM + " = '" + scan_DosageForm + "'";
-        Cursor c = db.rawQuery(countQuery, null);
+        String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_BRAND_NAME + " = '" + scan_BrandName + "' AND " + KEY_DOSAGE_FORM + " = '" + scan_DosageForm + "'";
+        Cursor c = db.rawQuery(selectQuery, null);
 
         if (c != null) {
-            return c.getInt(MedicationDatabaseSQLiteHandler.COL_TOTALDOSAGE);
+            return c.getInt(c.getColumnIndex("TotalDosage"));//MedicationDatabaseSQLiteHandler.COL_TOTALDOSAGE);
         } else{
             return 0;
         }
@@ -179,29 +210,77 @@ public class  MedicationDatabaseSQLiteHandler extends SQLiteOpenHelper{
         String where = KEY_ID + "=" + rowId;
         // Create row's data:
         ContentValues newValues = new ContentValues();
-        newValues.put(KEY_TOTALDOSAGE, saved_TotalDosage);
+        newValues.put(KEY_TOTAL_DOSAGE, saved_TotalDosage);
 
         Log.d("update Row", "Total Dosage saved = " + saved_TotalDosage);
         // Insert it into the database.
         return db.update(TABLE_MEDICATIONS, newValues, where, null) != 0;
     }
 
-        // Getting MedObjects Count
-		public int getMedObjectsCount() {
-            String countQuery = "SELECT  * FROM " + TABLE_MEDICATIONS;
-            SQLiteDatabase db = this.getReadableDatabase();
-            Cursor cursor = db.rawQuery(countQuery, null);
-            cursor.close();
+    // Getting MedObjects Count
+    public int getMedObjectsCount() {
+        String countQuery = "SELECT  * FROM " + TABLE_MEDICATIONS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
 
-            // return count
-            return cursor.getCount();
-        }
+        // return count
+        return cursor.getCount();
+    }
 
-        // Deleting single MedObject
-		public void deleteMedicationObject(Long MedId) {
-            SQLiteDatabase db = this.getWritableDatabase();
-            db.delete(TABLE_MEDICATIONS, KEY_ID + " = ?",
-                    new String[] { String.valueOf(MedId) });
-            db.close();
+    // Deleting single MedObject
+    public void deleteMedicationObject(Long MedId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_MEDICATIONS, KEY_ID + " = ?",
+                new String[]{String.valueOf(MedId)});
+        db.close();
+    }
+
+    public void addConsumptionDetails(String patientId, String brandName, String genericName, String consumedAt) {
+        SQLiteDatabase db = this.getWritableDatabase();
+                String selectQuery = "SELECT  * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_BRAND_NAME + " = '" + brandName + "' AND " +
+                        KEY_GENERIC_NAME + " = '" + genericName + "' AND " + KEY_PATIENT_ID + " = '" + patientId + "'";
+        Cursor c = db.rawQuery(selectQuery, null);
+        if (c != null) {
+            if (c.moveToFirst()) {
+                int colIndex = c.getColumnIndex(KEY_ID);
+                int med_id = c.getInt(colIndex);
+//        int med_id = c.getInt(0);
+                ContentValues values = new ContentValues();
+                values.put(KEY_MEDICATION_ID, med_id);
+                values.put(KEY_CONSUMED_AT, consumedAt);
+
+                // Inserting Row
+                db.insert(TABLE_CONSUMPTIONS, null, values);
+                db.close(); // Closing database connection
+            }
         }
+    }
+
+    public ConsumptionDetailsObject getConsumptionDetails(String patientId, String brandName, String genericName) {
+        int med_id = 0;
+        ConsumptionDetailsObject consumptionObject = null;
+        SQLiteDatabase db = this.getWritableDatabase();
+        String addQuery = "SELECT * FROM " + TABLE_MEDICATIONS + " WHERE " + KEY_BRAND_NAME + " = '" + brandName + "' AND " +
+                KEY_GENERIC_NAME + " = '" + genericName + "' AND " + KEY_PATIENT_ID + " = '" + patientId + "'";
+        Cursor c = db.rawQuery(addQuery, null);
+        if (c != null) {
+            if (c.moveToFirst()) {
+                med_id = c.getInt(c.getColumnIndex(KEY_ID));
+                String getQuery = "SELECT * FROM " + TABLE_CONSUMPTIONS + " WHERE " + KEY_MEDICATION_ID + " = " + med_id;
+                Cursor c2 = db.rawQuery(getQuery, null);
+                if (c2 != null) {
+                    if (c2.moveToFirst()) {
+                        String consumedAt = c2.getString(c2.getColumnIndex("ConsumedAt"));
+                        consumptionObject = new ConsumptionDetailsObject(med_id, consumedAt);
+                    }
+                }
+                c2.close();
+            }
+        }
+        c.close();
+        db.close(); // Closing database connection
+
+        return consumptionObject;
+    }
 }
